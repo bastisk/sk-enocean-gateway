a72d29d1fff8eca2877954ed2200e47922573353
Sebastian Kiepsch <sbki@mms-t-systems.de>
2019-06-20 13:29:34 +0200

    Added connection String to mlab

--- conf/mongodb.js
+++ conf/mongodb.js
@@ -1,5 +1,5 @@
 // default to a 'localhost' configuration:
-var connection_string = '192.168.2.153:27017/enocean';
+var connection_string = 'gw-user:oW7h^w\\USa3xKd.@=MeJ@cluster0-shard-00-00-ixuhy.mongodb.net:27017,cluster0-shard-00-01-ixuhy.mongodb.net:27017,cluster0-shard-00-02-ixuhy.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority';
 // if OPENSHIFT env variables are present, use the available connection info:
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
   connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
