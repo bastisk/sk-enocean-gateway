be426f178fc7305ff556aaaa47d15ec7365ed60e
bastisk <basti.sk@gmx.de>
2021-11-04 20:52:59 +0100

    reworks enocean monitor

--- enocean-monitor-new/index.js
+++ enocean-monitor-new/index.js
@@ -1,15 +1,24 @@
 const SerialPort = require("serialport");
+const mqtt = require("mqtt");
 const Enocean = require("enocean-js");
 const mqttclient = mqtt.connect("mqtt://localhost");
 const pretty = Enocean.pretty;
 const ESP3Parser = Enocean.ESP3Parser;
+const ESP3Transformer = Enocean.ESP3Transformer
+const parser = new ESP3Parser()
+const transformer = new ESP3Transformer()
 
 const port = new SerialPort("/dev/ttyAMA0", { baudRate: 57600 });
-const parser = new ESP3Parser();
-port.pipe(parser);
 
-parser.on("data", (data) => {
-  mqttclient.publish("data-unknown", JSON.stringify(telegram), (err) => {});
-});
+port.pipe(parser).pipe(transformer)
 
-parser.on("data", pretty.logESP3);
+
+transformer.on('data', async data => {
+    //console.log(data.decode("d2-50-00"))
+    if (data && data.constructor.name === "RadioERP1") {
+      if(data.teachIn){
+        var teachInInfo = data.teachInInfo
+        console.log(teachInInfo,data.RSSI)
+    }
+  })
+
