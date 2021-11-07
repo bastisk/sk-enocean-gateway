08d522d3fe80d460ef5fc742b2c11ec60ce62269
uebre <tom.schwabe@googlemail.com>
2021-11-07 14:56:07 +0100

    add routing, set strictPropertyInitialization=false

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { MonitorComponent } from './pages/monitor/monitor.component';
+import { PageNotFoundComponentComponent } from './pages/page-not-found-component/page-not-found-component.component'
+
+const routes: Routes = [
+  { path: 'devices', component: HomeComponent},
+  { path: 'monitor', component: MonitorComponent},
+  {
+    path: '',
+    redirectTo: '/devices',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
