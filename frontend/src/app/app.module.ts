dba10ea16f6c8ba190769b5b8ae9d0e94dfe8229
uebre <tom.schwabe@googlemail.com>
2021-11-07 14:19:57 +0100

    [frontend]: generate pages(home, monitor, page-not-found-component)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MonitorComponent } from './pages/monitor/monitor.component';
+import { PageNotFoundComponentComponent } from './pages/page-not-found-component/page-not-found-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MonitorComponent,
+    PageNotFoundComponentComponent
   ],
   imports: [
     BrowserModule,
